c4051a0abb8f91bda25aaa87b81ddd5a719ac2bf
alicerod26 <alicerod842@gmail.com>
2026-05-26 10:58:04 -0300

    primeiro comentário

---
+++ src/desvio3.c
@@ -0,0 +1,4 @@
+/*O programa deve receber 4 notas, realizar o cálculo da média e verificar se:
+    -A nota média for maior ou igual a 7, o aluno estará Aprovado; caso contrário estará Reprovado*/
+
+    
---
+++ src/mensagem.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("Olá seja bem vindo\n");
+
+    return 0;
+        
+}
