0fecd778bff78cef033efbfb3cb140b08571dac5
alicerod26 <alicerod842@gmail.com>
2026-05-26 11:55:45 -0300

    Desvio de fluxo usando if ... else ...

--- src/desvio3.c
+++ src/desvio3.c
@@ -1,4 +1,45 @@
 /*O programa deve receber 4 notas, realizar o cálculo da média e verificar se:
     -A nota média for maior ou igual a 7, o aluno estará Aprovado; caso contrário estará Reprovado*/
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int nota1, nota2, nota3, nota4;
     
+    // Comando System chama uma execução do sistema operacial
+    // que, neste caso será o comando clear
+
+    system("clear");
+
+    printf("Digite a primeira nota e tecle Enter\n");
+    scanf("%d",&nota1);
+
+    printf("Digite a primeira nota e tecle Enter\n");
+    scanf("%d",&nota2);
+
+    printf("Digite a primeira nota e tecle Enter\n");
+    scanf("%d",&nota3);
+
+    printf("Digite a primeira nota e tecle Enter\n");
+    scanf("%d",&nota4);
+
+    int media = nota1 + nota2 + nota3 + nota4;
+
+    if (media / 4 >= 7){ 
+        printf("Aprovado\n");
+    }
+
+    else{
+        printf("Reprovado\n");
+    }
+    
+    printf("\n########################################\n");
+    printf("Endereço de memória da variável nota1 %p \n", &nota1);
+    printf("Endereço de memória da variável nota2 %p \n", &nota2);
+    printf("Endereço de memória da variável nota3 %p \n", &nota3);
+    printf("Endereço de memória da variável nota4 %p \n", &nota4);
+    printf("Endereço de memória da variável media %p \n", &media);
+    
+    return 0;
+}
