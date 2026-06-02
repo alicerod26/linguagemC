5355f9ba3aa9428959f012cca5596308b8a9f449
alicerod26 <alicerod842@gmail.com>
2026-06-02 11:11:36 -0300

    Init: Inicializar o versionamento

---
+++ src/hello.c
@@ -0,0 +1,16 @@
+//Primeiro arquivo em linguagem C
+//Vamos importar a biblioteca base para executar o nosso programa
+//A biblioteca se chama stdio(std -> Standard | i -> input | o -> output)
+//Para importar, usaremos o comando #include
+#include <stdio.h>
+
+//Abaixo, iremos escrever os comandos que seram realmente executados
+//Utilizaremos a função main (principal) para iniciar a execição
+int main(){
+    // O comando printf exibe uma mensagem em tela de console
+    printf("Hello, World!\n");
+
+    // Informa ao compilador (tradutor) gcc que o programa acabou
+    return 0;
+
+}
