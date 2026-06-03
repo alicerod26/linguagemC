9b1dbc3f93243ee53d6fe711f099da7c7d66395b
alicerod26 <alicerod842@gmail.com>
2026-06-03 11:39:17 -0300

    Add:Arquivos de função

--- src/funcao2.c
+++ src/funcao2.c
@@ -6,16 +6,12 @@ int main(){
     int x , y , z;
 
     cabecalho();
-    printf("Digite um númeo inteiro e tecle Enter\n");
-    scanf("%d ,&X");
-    printf("Digite outro número inteiro e tecle Enter");
-    scanf("%d ,&y");
+    printf("Digite um número inteiro e tecle Enter\n");
+    scanf("%d",&x);
+    printf("Digite outro número inteiro e tecle Enter\n");
+    scanf("%d",&y);
     z = soma(x,y);
-    
-
-
-
-
-
-
-    
+    printf("O resultado da soma é %d\n",z);
+    rodape();
+    return 0;
+}
